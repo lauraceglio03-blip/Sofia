e1d3a294a8204c5904ec8c95d878c793c29bc98c
lauraceglio03-blip <laura.ceglio03@gmail.com>
2026-09-01 14:33:09 -0300

    Adicionando estrelas

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+for(let i=0;i<150;i++){
 
+    let estrela=document.createElement("div");
+
+    estrela.classList.add("estrela");
+
+    estrela.style.left=Math.random()*100+"vw";
+
+    estrela.style.top=Math.random()*100+"vh";
+
+    estrela.style.animationDelay=Math.random()*3+"s";
+
+    document.body.appendChild(estrela);
+
+}
